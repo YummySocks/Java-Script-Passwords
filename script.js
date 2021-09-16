eae2e3eab689b02b8ce836b496feed3e0f21e6e1
Socks <patricksutcliffe123@gmail.com>
2021-09-15 21:37:05 -0400

    added else if statements to gather user inputs

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  console.log("hey you clicked the button");
+
+  var passLength = prompt("How long do you want the Password");
+
+  if (passLength > 128){
+    window.alert("Please enter value below 129");
+    return;
+  } else if (passLength < 8){
+    window.alert("Please enter value above 7");
+    return;
+  }else{
+    
+  }
+
+  var passSpecial = confirm("do you want special characters?");
+  if (passSpecial){
+    passSpecial=true;
+  } else {
+    passSpecial=false;
+  }
+
+  var passLower = confirm("Do you want lower case characters?");
+  if(passLower){
+    passLower=true;
+  }else{
+    passLower=false;
+  }
+
+  var passUpper = confirm("Do you want upper case characters?");
+  if(passUpper){
+    passUpper=true;
+  }else{
+    passUpper=false;
+  }
+
+  var passNum = confirm("Do you want numbers?")
+  if(passNum){
+    passNum=true;
+  }else{
+    passNum=false;
+  }
+
+  console.log(passLength + passSpecial)
+  return "Generated Password";
+};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
