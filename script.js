3654303a2ff7dfef0a2e4dad3413db10ab04e295
Socks <patricksutcliffe123@gmail.com>
2021-09-15 22:03:13 -0400

    added a password generate for loop

--- script.js
+++ script.js
@@ -18,34 +18,38 @@ function generatePassword(){
 
   var passSpecial = confirm("do you want special characters?");
   if (passSpecial){
-    passSpecial=true;
+    passSpecial="!@#$%^&*()_+";
   } else {
     passSpecial=false;
   }
 
   var passLower = confirm("Do you want lower case characters?");
   if(passLower){
-    passLower=true;
+    passLower="abcdefghijklmnopqrstuvwxyz";
   }else{
     passLower=false;
   }
 
   var passUpper = confirm("Do you want upper case characters?");
   if(passUpper){
-    passUpper=true;
+    passUpper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }else{
     passUpper=false;
   }
 
   var passNum = confirm("Do you want numbers?")
   if(passNum){
-    passNum=true;
+    passNum="1234567890";
   }else{
     passNum=false;
   }
 
-  console.log(passLength + passSpecial)
-  return "Generated Password";
+  for (var i=0; i< passLength; i++){
+    var randomNumber = Math.floor(Math.random() * (passSpecial.length + passLower.length + passUpper.length + passNum.length));
+    password += passSpecial.substring(randomNumber, randomNumber + 1) + passLower.substring(randomNumber, randomNumber + 1) + passUpper.substring(randomNumber, randomNumber + 1) + passNum.substring(randomNumber, randomNumber + 1);
+  }
+
+  return password;  
 };
 
 // Write password to the #password input
