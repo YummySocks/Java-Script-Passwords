d7ef2fec430f2c5a348119eda8dc987aae532bd0
Socks <patricksutcliffe123@gmail.com>
2021-09-16 14:34:10 -0400

    still messing around with syntax for array and for loop

--- script.js
+++ script.js
@@ -1,74 +1,76 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 var chosenChars = [];
 function generatePassword(){
-  console.log("hey you clicked the button");
+  console.log('hey you clicked the button');
 
-  var passLength = prompt("How long do you want the Password");
+  var passLength = prompt('How long do you want the Password');
 
   if (passLength > 128){
-    window.alert("Please enter value below 129");
+    window.alert('Please enter value below 129');
     return;
   } else if (passLength < 8){
-    window.alert("Please enter value above 7");
+    window.alert('Please enter value above 7');
     return;
   }else{
     
   }
 
-  var passSpecial = confirm("do you want special characters?");
+  var passSpecial = confirm('do you want special characters?');
   if (passSpecial){
-    passSpecial=["!","@","#","$","%","^","&","*","(",")","_","+",];
+    passSpecial=('!','@','#','$','%','^','&','*','(',')','_','+');
     chosenChars.push(passSpecial);
   } else {
     
   }
 
-  var passLower = confirm("Do you want lower case characters?");
+  var passLower = confirm('Do you want lower case characters?');
   if(passLower){
-    passLower=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+    passLower=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
     chosenChars.push(passLower);
   }else{
     
   }
 
-  var passUpper = confirm("Do you want upper case characters?");
+  var passUpper = confirm('Do you want upper case characters?');
   if(passUpper){
-   passUpper= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+   passUpper= ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
    chosenChars.push(passUpper);
   }else{
     
   }
 
-  var passNum = confirm("Do you want numbers?")
+  var passNum = confirm('Do you want numbers?')
   if(passNum){
-    passNum=["1","2","3","4","5","6","7","8","9","0"];
+    passNum=('1','2','3','4','5','6','7','8','9','0');
     chosenChars.push(passNum);
   }else{
     
   }
   password = [];
+  console.log(chosenChars);
   // for (var i=0; i< passLength; i++){
   //   var randomNumber = Math.floor(Math.random() * (passSpecial.length + passLower.length + passUpper.length + passNum.length));
   //   password += passSpecial.substring(randomNumber, randomNumber + 1) + passLower.substring(randomNumber, randomNumber + 1) + passUpper.substring(randomNumber, randomNumber + 1) + passNum.substring(randomNumber, randomNumber + 1);
   // }
+  
+  for (var i=0; i < passLength; i++){
   var randomNumber = Math.floor(Math.random() * chosenChars.length);
-
-for (var i=0; i < passLength; i++){
-    password = password.push(chosenChars[randomNumber]);
-    password += password.splice(randomNumber);
+  password += chosenChars.splice(randomNumber); 
 }
+// password.push(chosenChars[randomNumber])
+  console.log([password]);
   return password;  
 };
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
